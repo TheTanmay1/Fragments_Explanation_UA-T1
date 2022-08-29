1297625206ffc521cca9dcfca57db3581d6bffb1
thetanmay1 <foundertanmay@gmail.com>
2022-08-29 06:21:40 +0530

    finally_completed_6_21am_29august2022

--- app/src/main/java/com/unwiringapps/letsfeelfragments/MainActivity.kt
+++ app/src/main/java/com/unwiringapps/letsfeelfragments/MainActivity.kt
@@ -2,10 +2,37 @@ package com.unwiringapps.letsfeelfragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.unwiringapps.letsfeelfragments.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnRemoveButtonTapListener {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button1.setOnClickListener {
+
+
+            if (savedInstanceState == null)
+            {
+                supportFragmentManager.beginTransaction()
+                    .add(R.id.framelayout1, FragmentLayout1.newInstance(), "MyTag").commit()
+            }
+        }
+    }
+
+    override fun onRemoveButtonTapped() {
+
+        val frag = supportFragmentManager.findFragmentByTag("MyTag")
+
+        if ( frag!= null ) {
+            supportFragmentManager.beginTransaction().remove(frag).commit()
+        }
+
+
     }
 }
